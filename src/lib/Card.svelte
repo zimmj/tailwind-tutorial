e9b48be53cb4e1987d9a222db32c27fab4e70d9e
zimmj <33086958+zimmj@users.noreply.github.com>
2024-01-25 17:06:15 +0100

    ~feat(utility-classes): Base concept of utility classes
    Show the base concept of utility classes with a simple exercise

---
+++ src/lib/Card.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<div class="holder">
+	<div class="card">
+		<div class="card-header">
+			<h2>{title}</h2>
+		</div>
+		<div class="card-body">
+			<slot />
+		</div>
+	</div>
+</div>
+
+<style>
+	.holder {
+		display: flex;
+		justify-content: center;
+	}
+	h2 {
+		font-size: 32px;
+	}
+	.card {
+		border: 1px solid #ccc;
+		border-radius: 15px;
+		margin: 24px;
+		padding: 16px;
+	}
+	.card-header {
+		border-bottom: 1px solid #ccc;
+	}
+	.card-body {
+		margin: 24px 0;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import CardTailwind from '$lib/Card-Tailwind.svelte';
+	import Card from '$lib/Card.svelte';
+</script>
+
+<div>
+	<Card title="Tailwind Tutorial">
+		<div>
+			<p>This is a simple tutorial, don't expect to much</p>
+		</div>
+		<div>
+			<p>But after you should have learned some basic css.</p>
+			<p>And how to use <b>Tailwind</b> combiened with your knowledge of css.</p>
+			<p><b>Tailwind</b> does not <mark>remove</mark> the requirement, that you know css.</p>
+		</div>
+	</Card>
+
+	<CardTailwind title="Tailwind Tutorial">
+		<div>
+			<p>This is a simple tutorial, don't expect to much</p>
+		</div>
+		<div>
+			<p>But after you should have learned some basic css.</p>
+			<p>And how to use <b>Tailwind</b> combiened with your knowledge of css.</p>
+			<p><b>Tailwind</b> does not <mark>remove</mark> the requirement, that you know css.</p>
+		</div>
+	</CardTailwind>
+</div>
